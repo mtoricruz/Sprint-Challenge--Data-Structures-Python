c877a43067046fc124f5e5076ba578e8b065c5cb
toricruzmendiola <mtoricruz@gmail.com>
2020-07-17 09:29:57 -0700

    names file complete. went from 7+ second run time to 0.13

--- names/binary_search_tree.py
+++ names/binary_search_tree.py
@@ -1,5 +1,5 @@
-from queue import Queue
-from stack import Stack
+# from queue import Queue
+# from stack import Stack
 
 """
 Binary search trees are a data structure that enforce an ordering over 
@@ -63,103 +63,103 @@ class BSTNode:
                 return self.right.contains(target)
 
     # Return the maximum value found in the tree
-    def get_max(self):
-        if self.right:
-            return self.right.get_max()
-        else:
-            return self.value
+#     def get_max(self):
+#         if self.right:
+#             return self.right.get_max()
+#         else:
+#             return self.value
 
-    # Call the function `fn` on the value of each node
-    def for_each(self, fn):
-        fn(self.value)
-        if self.left:
-            self.left.for_each(fn)
-        if self.right:
-            self.right.for_each(fn)
+#     # Call the function `fn` on the value of each node
+#     def for_each(self, fn):
+#         fn(self.value)
+#         if self.left:
+#             self.left.for_each(fn)
+#         if self.right:
+#             self.right.for_each(fn)
 
-    # Part 2 -----------------------
+#     # Part 2 -----------------------
 
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        # P - Traverse BST on left side
-        # as long as a left side exist
-        # print root value
-        if self.left is not None:
-            self.left.in_order_print(node)
-        print(self.value)
-        if self.right is not None:
-            self.right.in_order_print(node)
+#     # Print all the values in order from low to high
+#     # Hint:  Use a recursive, depth first traversal
+#     def in_order_print(self, node):
+#         # P - Traverse BST on left side
+#         # as long as a left side exist
+#         # print root value
+#         if self.left is not None:
+#             self.left.in_order_print(node)
+#         print(self.value)
+#         if self.right is not None:
+#             self.right.in_order_print(node)
          
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-    def bft_print(self, node):
-        if node is None:
-            return
-        # P - Create queue
-        queue = Queue()
-        # add root to queue
-        queue.enqueue(node)
-        # while queue is not empty
-        while len(queue) > 0:
-            # node = pop head of queue
-            node = queue.dequeue()
-            # DO THE THING YOU NEED (in this case it is to print)
-            print(node.value)
-            # add children of node to queue
-            if node.left:
-                queue.enqueue(node.left)
-            if node.right:
-                queue.enqueue(node.right)
+#     # Print the value of every node, starting with the given node,
+#     # in an iterative breadth first traversal
+#     def bft_print(self, node):
+#         if node is None:
+#             return
+#         # P - Create queue
+#         queue = Queue()
+#         # add root to queue
+#         queue.enqueue(node)
+#         # while queue is not empty
+#         while len(queue) > 0:
+#             # node = pop head of queue
+#             node = queue.dequeue()
+#             # DO THE THING YOU NEED (in this case it is to print)
+#             print(node.value)
+#             # add children of node to queue
+#             if node.left:
+#                 queue.enqueue(node.left)
+#             if node.right:
+#                 queue.enqueue(node.right)
 
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
-    def dft_print(self, node):
-        if node is None:
-            return
-        # P - Create stack
-        stack = Stack()
-        # add root to stack
-        stack.push(node)
-        # while stack is not empty
-        while len(stack) > 0:
-            # node = pop top off the stack
-            node = stack.pop()
-            # DO THE THING YOU NEED (in this case it is to print)
-            print(node.value)
-            # add children of node to stack
-            if node.left:
-                stack.push(node.left)
-            if node.right:
-                stack.push(node.right)
+#     # Print the value of every node, starting with the given node,
+#     # in an iterative depth first traversal
+#     def dft_print(self, node):
+#         if node is None:
+#             return
+#         # P - Create stack
+#         stack = Stack()
+#         # add root to stack
+#         stack.push(node)
+#         # while stack is not empty
+#         while len(stack) > 0:
+#             # node = pop top off the stack
+#             node = stack.pop()
+#             # DO THE THING YOU NEED (in this case it is to print)
+#             print(node.value)
+#             # add children of node to stack
+#             if node.left:
+#                 stack.push(node.left)
+#             if node.right:
+#                 stack.push(node.right)
         
 
-    # Stretch Goals -------------------------
-    # Note: Research may be required
+#     # Stretch Goals -------------------------
+#     # Note: Research may be required
 
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
+#     # Print Pre-order recursive DFT
+#     def pre_order_dft(self, node):
+#         pass
 
-    # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
+#     # Print Post-order recursive DFT
+#     def post_order_dft(self, node):
+#         pass
 
-bst = BSTNode(1)
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
-bst.bft_print(bst)
-bst.dft_print(bst)
-# print("elegant methods")
-# print("pre order")
-# bst.pre_order_dft(bst)
-# print("in order")
-# bst.in_order_dft(bst)
-# print("post order")
-# bst.post_order_dft(bst)
+# bst = BSTNode(1)
+# bst.insert(8)
+# bst.insert(5)
+# bst.insert(7)
+# bst.insert(6)
+# bst.insert(3)
+# bst.insert(4)
+# bst.insert(2)
+# bst.bft_print(bst)
+# bst.dft_print(bst)
+# # print("elegant methods")
+# # print("pre order")
+# # bst.pre_order_dft(bst)
+# # print("in order")
+# # bst.in_order_dft(bst)
+# # print("post order")
+# # bst.post_order_dft(bst)
--- names/names.py
+++ names/names.py
@@ -1,4 +1,6 @@
 import time
+# import BST because we have to traverse through entire name files,
+# not just search for names
 from binary_search_tree import BSTNode
 
 start_time = time.time()
@@ -13,11 +15,21 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+# initialize BST
+name_nodes = BSTNode(names_1[0])
+
 # Replace the nested for loops below with your improvements
+# traverse through 1st name file 
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    # and add names to BST which is assigned name_nodes
+    name_nodes.insert(name_1)
+
+# traverse through 2nd name file 
+for name_2 in names_2:
+    # if name_nodes contains name_2 as well
+    if name_nodes.contains(name_2):
+        # add that name to the duplicates list [should be 64]
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
